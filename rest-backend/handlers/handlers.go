3061a69634689fda1d8b8dff2f74833d0f75ca0c
Casey Wylie <cmwylie19@gmail.com>
2021-10-05 19:12:14 -0400

    http service

--- rest-backend/handlers/handlers.go
+++ rest-backend/handlers/handlers.go
@@ -2,10 +2,11 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
-	"github.com/cmwylie19/grpc-vs-rest/backend/controllers"
-	"github.com/cmwylie19/grpc-vs-rest/backend/models"
+	"github.com/cmwylie19/grpc-vs-rest/rest-backend/controllers"
+	"github.com/cmwylie19/grpc-vs-rest/rest-backend/models"
 )
 
 func GetRemote(w http.ResponseWriter, r *http.Request) {
@@ -16,5 +17,38 @@ func GetRemote(w http.ResponseWriter, r *http.Request) {
 		controllers.GetError(err, w)
 		return
 	}
+	w.Write(result)
+}
+
+func CreateImage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var img models.Image
+
+	_ = json.NewDecoder(r.Body).Decode(&img)
+	fmt.Println("Image: ", img)
+	err1 := controllers.CreateImage(img)
+	if err1 != nil {
+		controllers.GetError(err1, w)
+		return
+	}
+	result, err := json.Marshal(img)
+	if err != nil {
+		controllers.GetError(err, w)
+		return
+	}
+	w.Write(result)
+}
+
+func GetImage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	fmt.Println("PArams: ", r.URL.Path[1:])
+	img, err := controllers.GetImage("archie.jpg")
+	if err != nil {
+		controllers.GetError(err, w)
+		return
+	}
+
+	result, _ := json.Marshal(img)
 	w.Write(result)
 }
--- rest-backend/main.go
+++ rest-backend/main.go
@@ -5,12 +5,14 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/cmwylie19/grpc-vs-rest/http-backend/handlers"
-	"github.com/cmwylie19/grpc-vs-rest/http-backend/middleware"
+	"github.com/cmwylie19/grpc-vs-rest/rest-backend/handlers"
+	"github.com/cmwylie19/grpc-vs-rest/rest-backend/middleware"
 )
 
 func HandleRequests() {
-	http.HandleFunc("/backend/healthz", middleware.LoggingMiddleware(handlers.GetRemote))
+	http.HandleFunc("/rest/healthz", middleware.LoggingMiddleware(handlers.GetRemote))
+	http.HandleFunc("/rest/img/create", middleware.LoggingMiddleware(handlers.CreateImage))
+	http.HandleFunc("/rest/img/get", middleware.LoggingMiddleware(handlers.GetImage))
 	fmt.Println("Serving from :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
